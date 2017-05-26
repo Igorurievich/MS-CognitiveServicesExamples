0dc6f0589a17c45bcfb6482bd91b2f3e78e7a114
SIIPOLSKA\ibodia <ibodia@pl.sii.eu>
2017-05-26 16:07:03 +0200

    Initial commit.

---
+++ ComputerVisionDemo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.ProjectOxford.Vision.Contract;
+using Microsoft.ProjectOxford.Vision;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System;
+using System.Windows.Controls;
+
+namespace ComputerVisionDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<AnalysisResult> AnalyzeUrl(string imageUrl)
+        {
+            string SubscriptionKey = "";
+            string APIRoot = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0";
+
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey, APIRoot);
+
+            VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags };
+            AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageUrl, visualFeatures);
+            return analysisResult;
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri(@"");
+            b.EndInit();
+
+            var image = sender as Image;            
+            image.Source = b;
+        }
+
+        private async void RecognizeAsyncClick(object sender, RoutedEventArgs e)
+        {
+            runAsyncRecog.IsEnabled = false;
+
+            var slowTask = await AnalyzeUrl(@"https://pp.userapi.com/c836231/v836231496/2cc31/7zjQanEbu7Q.jpg");
+
+            ImagesRichTextBox.AppendText(slowTask.Faces.ToString());
+
+            runAsyncRecog.IsEnabled = true;
+        }
+    }
+}
---
+++ VisionAPI Demo/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.ProjectOxford.Vision.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionAPI_Demo
+{
+    //C:\Users\ibodia\Desktop\123.jpg
+    static class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter image file path: ");
+            string imageFilePath = @"C:\Users\ibodia\Desktop\123.jpg";
+
+            MakeAnalysisRequest(imageFilePath);
+
+            Console.WriteLine("\n\n\nHit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        static async void MakeAnalysisRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "");
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "visualFeatures=Categories&language=en";
+            string uri = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze?" + requestParameters;
+
+            Console.WriteLine(uri);
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+
+
+                var resp = await client.GetStreamAsync(uri);
+                Console.WriteLine("Is succes ?" + response.IsSuccessStatusCode.ToString());
+                //Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+                Stream receiveStream = await response.Content.ReadAsStreamAsync();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                Console.WriteLine(readStream.ReadToEnd());
+
+            }
+        }
+    }
+}
